5889c5ce686d92a6efbe1d0297f039887943fd0b
ARHL2023 <andyholguinluna@gmail.com>
2024-04-01 11:35:12 -0500

    descomprimiendo rar

---
+++ LaTecnologiaAvanza/2PlantillasThymeleafOperacionesCRUD/SERVICIO-TECNICO/src/test/java/com/tecnico/servicios/ServicioTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnico.servicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioTecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
